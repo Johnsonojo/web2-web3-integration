715ac28b2480db4225b33c8ad92a0469ce87e871
johnsonojo <johnsonojo89@gmail.com>
2024-11-13 13:54:58 +0100

    updated entry file

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,40 @@
 import { ApolloServer } from "apollo-server-express";
-import * as dotenv from "dotenv";
+import dotenv from "dotenv";
 import express from "express";
 import { resolvers } from "./graphql/resolvers";
 import { typeDefs } from "./graphql/schema";
 
 dotenv.config();
 
-const startServer = async () => {
-  const app = express();
+const PORT = process.env.PORT || 4000;
+const HOST =
+  process.env.NODE_ENV === "production" ? process.env.HOST_URL : "localhost";
+const PROTOCOL = HOST === "localhost" ? "http" : "https";
 
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    cache: "bounded",
-  });
+async function startServer() {
+  try {
+    const app = express();
 
-  await server.start();
-  server.applyMiddleware({ app });
-  const port = process.env.PORT || 4000;
-  const host = process.env.NODE_ENV === 'production' ? process.env.HOST_URL : 'localhost';
-  const protocol = host === 'localhost' ? 'http' : 'https';
-  app.listen({ port }, () =>
-    console.log(`Server ready at ${protocol}://${host}:${port}${server.graphqlPath}`)
-  );
-};
+    const apolloServer = new ApolloServer({
+      typeDefs,
+      resolvers,
+      cache: "bounded",
+    });
 
-startServer().catch((error) => {
-  console.error("Error starting the server:", error);
-});
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app });
+
+    app.listen(PORT, () => {
+         if (process.env.NODE_ENV === 'production') {
+           console.log(`🚀 Server ready at ${apolloServer.graphqlPath}`);
+         } else {
+           console.log(`🚀 Server ready at ${PROTOCOL}://${HOST}:${PORT}${apolloServer.graphqlPath}`);
+         }
+       });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+    process.exit(1);
+  }
+}
+
+startServer();
